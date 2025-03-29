21fa708a26a4b9cf1a258498e9ecfdd4bb533519
Vitor <vitoryanfma@gmail.com>
2025-03-29 15:20:02 -0300

    feat(projects): creating dialog projects, calling by function using material dialog

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,13 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+
+// Interface
 import { IProjects } from '../../interfaces/IProjects.interface';
+
+// Material
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { EDialogPanelClass } from '../../enums/EDialogPanelClass.enum';
+
+// Dialog
+import { DialogProjectsComponent } from '../../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
   arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/projects/marvel.jpg',
@@ -24,4 +34,11 @@ export class ProjectsComponent {
       ]
     }
   ])
+
+  openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    })
+  }
 }
